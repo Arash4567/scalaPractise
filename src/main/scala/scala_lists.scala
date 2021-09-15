fbcbc6a6e383fb4a3f14013ee172e3172ffe6868
Arash4567 <shomoyxhacker007@mail.ru>
2021-09-15 15:10:55 +0500

    list

--- src/main/scala/scala_lists.scala
+++ src/main/scala/scala_lists.scala
@@ -9,11 +9,20 @@ object scala_lists {
   val item3: List[Boolean] = List(true, false, true, true)
   val item4: List[Double] = List(1.2123, 2.1556, 3.6)
   val item5: List[Any] = List(1.2123, 56, true, "Bye!")
+  val item6: List[String] = "Jan" :: "Feb" :: "Mar" :: Nil
 
+  //  Creating empty List
+  val emptyList: List[Nothing] = Nil
+
+  //  Creating Number List
+  val numList: List[Int] = List.range(1,11)
+  val numList2: List[Int] = List.range(1,11,3)
   def main(args: Array[String]): Unit = {
     val result = 111 :: item
     println(result)
     println(item)
+    println(item.size)
+    println(item.length)
     println(item.head)
     println(item.tail)
     println(item.isEmpty)
@@ -25,12 +34,17 @@ object scala_lists {
     println(item.sum)
     println(item.intersect(itemClone))
     val item5 = List.fill(5)(3)
-//                        |  |
-//                   repeat  argument
+    //                    |  |
+    //               repeat argument
     println(item5)
     var yig: Int = 0
     item.foreach(yig += _)
     println(yig)
-    
+    println(item6)
+    println(emptyList)
+    println(numList)
+    println(numList2)
+    println(item6.zipWithIndex)
+    println(item6.zip(numList2))
   }
 }

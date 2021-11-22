ff856c9e7b5ab4525391347210e6c4e772074c82
Arash4567 <shomoyxhacker007@mail.ru>
2021-11-22 15:23:40 +0300

    added Xulosa

---
+++ src/main/scala/scalaFun/LessonFour.scala
@@ -0,0 +1,34 @@
+package scalaFun
+
+object LessonFour extends App {
+  //  Biz birlamchi konstruktorni nol, bir yoki bir nechta parametr bilan belgilashimiz mumkin.
+  class Greeting {}
+
+  /*val DefaultCrustSize = 30
+  val DefaultCrustType = "YUPQA"
+
+  // asosiy konstruktor
+  class Pizza (var crustSize: Int, var crustType: String) {
+
+    // bir agumentli yordamchi konstruktor
+    def this(crustSize: Int) = {
+      this(crustSize, DefaultCrustType)
+    }
+    def this(crustSize: Int, crustType: String) = {
+      this(crustSize, crustType)
+    }
+
+    // bir agumentli yordamchi konstruktor
+    def this(crustType: String) = {
+      this(DefaultCrustSize, crustType)
+    }
+
+    // nol agumentli yordamchi konstruktor
+    def this() = {
+      this(DefaultCrustSize, DefaultCrustType)
+    }
+
+    override def toString = s"Pitzamizning diameteri $crustSize sm va $crustType hamirli"
+
+  }*/
+}
